30a1c583ef954590b4a43c4ece8134c32cab769f
sethjuarez <me@sethjuarez.com>
2021-10-22 18:08:24 -0700

    added key replacement for yml files

---
+++ cloud/replace.py
@@ -0,0 +1,9 @@
+import sys
+args = sys.argv[2:]
+template = sys.argv[1]
+kwargs = {}
+for i in range(int(len(args) / 2)):
+    kwargs[args[2*i].replace('--', '')] = args[2*i+1]
+with open(template, 'r') as f:
+    t = f.read().format(**kwargs)
+    print(t)
